a6ed66a22f87d0b651a9d1eec363ec4a561e4c86
Aidan <aidanmurphy1@hotmail.co.uk>
2017-04-09 12:50:24 -0400

    Added eye tracking analysis

--- MF3D_AnimatedInteractionDemo.m
+++ MF3D_AnimatedInteractionDemo.m
@@ -16,21 +16,43 @@ Display             = DisplaySettings(Stereo);      % Get display settings
 Display.Rect        = Display.Rect/2                % For debugging...
 Display.Background  = [0 0 0];                      % Set background color (RGB)
 Display.Imagingmode = [];                           
-%HideCursor;
-KbName('UnifyKeyNames');
-Screen('Preference', 'VisualDebugLevel', 1);     
+%HideCursor;                                         % Don't show cursor on screen
+KbName('UnifyKeyNames');                            % Prepare keyboard
+Screen('Preference', 'SkipSyncTests', 1);           % Skip PTB sync tests for now...
+Screen('Preference', 'VisualDebugLevel', 1);        % Default PTB window color is black
 [Display.win, rect2] = Screen('OpenWindow', Display.ScreenID, Display.Background, Display.Rect,[],[], Display.Stereomode, [], Display.Imagingmode);
 Screen('BlendFunction', Display.win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);    	% Enable alpha channel   
+ 
 
 %============= Load animation data
 AnimationDir    = '/Volumes/Seagate Backup 1/NIH_PhD_nonthesis/7. 3DMacaqueFaces/AnimationDemos';
+AnimationDir    = '/Users/aidanmurphy/Desktop/MacaqueLookAtDemo/';
 FileFormat      = 'avi';
 ClipFiles       = wildcardsearch(AnimationDir, sprintf('*.%s',FileFormat));
-Clips           = MF3D_LoadClips(ClipFiles, Display.win);
+Clips           = MF3D_LoadClips(ClipFiles, Display.win, 1);
+
+%============= Generate blink sequence
+Blink.Duration  = Clips(1).TotalDuration;
+Blink.NoFrames  = numel(Clips(1).framehandles);
+Blink.Total     = 10^5;
+Blink.Mu        = 0.2;                                              % Mean blink rate (Hz)
+Blink.Sigma     = 0.2;                                              % Standard deviation of blink rate (Hz)
+Blink.IBIs      = lognrnd(Blink.Mu,Blink.Sigma,[1,Blink.Total])+2;  % Inter-blink intervals (seconds)
+Blink.Onsets    = cumsum(Blink.IBIs);                               % Blink onset times (seconds)
+Blink.Count     = 1;
+Blink.Frame     = 0;
+
+%============= Generate LookAt timing
+LookAt.On           = 0;
+LookAt.FPA          = 120;                                      % Frames per action sequence
+LookAt.Order        = [5,2,1,4,7,8];
+LookAt.StartFrame   = [1, 1+(1:numel(LookAt.Order)-1)*LookAt.FPA];
+LookAt.EndFrame     = LookAt.StartFrame+LookAt.FPA/2;
 
 
-SourceRect      = Display.Rect;
-DestRect        = Display.Rect;
+rect2
+Display.Rect
+DestRect        = Display.Rect*2;
 
 MaskFile        = '/Volumes/Seagate Backup 1/NIH_PhD_nonthesis/7. 3DMacaqueFaces/AnimationDemos/TargetMask2.png';
 MaskIm          = imread(MaskFile);
@@ -45,42 +67,74 @@ for g = 1:2
 end
 
 %============= Begin demo
-FrameOnset = [];
-while 1
+MovieTex = Clips(1).framehandles(1);
+FrameOnset  = [];
+EndTrial    = 0;
+StartTime   = GetSecs;
+while EndTrial == 0
+    
     %============== Check gaze/ mouse location
-    [x,y]           = GetMouse(Display.win);                                  	% Get cursor coordinates (pixels relative to upper left corner)
+    [x,y,b]        	= GetMouse(Display.win);                                  	% Get cursor coordinates (pixels relative to upper left corner)
     EyePos          = [ceil(x),ceil(y)];                                        % Round coordinates to nearest pixel
+    if any(b)                                                                   % If a mouse button was pressed...
+        fprintf('Mouse pos = %d, %d\n', EyePos);
+    end
     for e = 1:2
-        if any(EyePos(e) < 1)                                                      % Constrain possible gaze positions based on screen resolution
+        if any(EyePos(e) < 1)                                                	% Constrain possible gaze positions based on screen resolution
             EyePos(e) = 1;
         end
         if EyePos(e) > Display.Rect(2+e)
             EyePos(e) = Display.Rect(2+e);
         end
     end
-    RegionIndex     = MaskIm(EyePos(2),EyePos(1));                             	% Query which region of the image the cursor is over
+    RegionIndex     = MaskIm(EyePos(2),EyePos(1));                                      % Query which region of the image the cursor is over
     GazeDestRect	= CenterRectOnPoint(GazeRectSize,EyePos(1),EyePos(2));
-    if RegionIndex > 0                                                          % If cursor is not over a target...
+    if RegionIndex > 0                                                                  % If cursor is currently over a target...
         DrawMousePos    = 1;
-        g = 2;
+        GazeColorIndx   = 2;
+        if LookAt.On == 0
+            LookAt.On = 1;
+            LookAt.CurrentFrame = LookAt.StartFrame(find(LookAt.Order==RegionIndex));   % Set start frame for saccade
+        elseif LookAt.On == 1
+            LookAt.CurrentFrame = LookAt.CurrentFrame+1;
+            if LookAt.CurrentFrame >= LookAt.StartFrame(find(LookAt.Order==RegionIndex))+LookAt.FPA-1
+                LookAt.On = 0;
+            end
+        end
+        MovieTex = Clips(2).framehandles(LookAt.CurrentFrame);
     else
         DrawMousePos = 1;
-        g = 1;
+        GazeColorIndx = 1;
     end
+    
+    %============ Check blink action
+    if Blink.Frame == 0 && LookAt.On == 0                           % If no other action is currently in progress...
+        if (GetSecs-StartTime) > Blink.Onsets(Blink.Count)          % If next blink onset time has arrived...
+            Blink.Count = Blink.Count+1;                            % Advance blink count
+            Blink.Frame = Blink.Frame+1;                            % Advance blink frame count
+        end
+    elseif Blink.Frame > 0                                          % If current blink frame is not zero
+        MovieTex    = Clips(1).framehandles(Blink.Frame);           % Get current blink frame
+        Blink.Frame = Blink.Frame+1;                                % Advance blink frame count
+        if Blink.Frame > Blink.NoFrames                             % If blink has completed...
+            Blink.Frame = 0;                                        % Reset blink frame count to zero
+        end
+    end
+        
 %     Screen('PlayMovie',Clips(1).handle,1,[],Movie.Volume);
 %     Screen('SetmovieTimeIndex',Clips(1).handle,StartTime,1); 
 %     MovieTex = Screen('GetMovieImage', Display.win, Clips(1).handle, 1);
-    MovieTex = Clips(1).framehandles(1);
+
     
     %============== Draw frame
     for Eye = 1:2                                                                   % For each eye...
         currentbuffer = Screen('SelectStereoDrawBuffer', Display.win, Eye-1);       % Select buffer for each eye
         if exist('MovieTex','var')
-            Screen('DrawTexture', Display.win, MovieTex, SourceRect, DestRect);   	% Draw frame for current eye
+            Screen('DrawTexture', Display.win, MovieTex, [], DestRect);             % Draw frame for current eye
         end
         %Screen('DrawTexture', Display.win, MaskTex, [], Display.Rect);            	% Draw mask texture to screen
         if DrawMousePos == 1                                                        % If cursor is on target region...
-            Screen('DrawTexture', Display.win, GazeMarker(g), [], GazeDestRect);  	% Draw eye position
+            Screen('DrawTexture', Display.win, GazeMarker(GazeColorIndx), [], GazeDestRect);  	% Draw eye position
         end
     end
     [VBL FrameOnset(end+1)] = Screen('Flip', Display.win);                          % Flip frame to screen
@@ -88,22 +142,25 @@ while 1
     %============== Check user input
     [keyIsDown,secs,keyCode] = KbCheck;                                             % Check keyboard for 'escape' press        
     if keyIsDown && keyCode(KbName('Escape')) == 1                                  % Press Esc for abort
+        EndTrial = 1;
         break
     end
     
     
 end
+disp('Exited loop')
 
 %============== Perform clean up
-Screen('CloseAll');      
+Screen('CloseAll');  
+sca
 %ShowCursor;
 
 %============== Show some stats about software performance
 Frametimes      = diff(FrameOnset);
 meanFrameRate   = mean(Frametimes(2:end))*1000;
 semFrameRate    = (std(Frametimes(2:end))*1000)/sqrt(numel(Frametimes(2:end)));
-fprintf('Frames shown............%.0f\n', numel(Frametimes));
-fprintf('Mean frame duration.....%.0f ms +/- %.0f ms\n', meanFrameRate, semFrameRate);
-fprintf('Max frame duration......%.0f ms\n', max(Frametimes)*1000);
+fprintf('Frames shown............%d\n', numel(Frametimes));
+fprintf('Mean frame duration.....%.2f ms +/- %.2f ms\n', meanFrameRate, semFrameRate);
+fprintf('Max frame duration......%.2f ms\n\n', max(Frametimes)*1000);
 
 
--- MF3D_LoadClips.m
+++ MF3D_LoadClips.m
@@ -9,6 +9,11 @@ function Clips = MF3D_LoadClips(ClipFiles, PTBwin, verbose)
 if nargin < 3
     verbose = 0;
 end
+if nargin < 2
+    Method = 2;
+else
+    Method = 1;
+end
 
 %=========== Check memory usage
 for c = 1:numel(ClipFiles)
@@ -17,48 +22,53 @@ for c = 1:numel(ClipFiles)
     [Clips(c).Dir,Clips(c).Filename,Clips(c).Format] = fileparts(ClipFiles{c});
     Clips(c).MemMB          = temp.bytes/10^6;
 end
-if verbose ==1
-    resp = questdlg(sprintf('Total memory required = %.2f MB. Proceed with loading movie files?', sum([Clips.MemMB])), 'Memory Required','Yes','No','Yes');
-    if strcmp(resp, 'No')
-        Clips = [];
-        return
-    end
-end
+% if verbose ==1
+%     resp = questdlg(sprintf('Total memory required = %.2f MB. Proceed with loading movie files?', sum([Clips.MemMB])), 'Memory Required','Yes','No','Yes');
+%     if strcmp(resp, 'No')
+%         Clips = [];
+%         return
+%     end
+% end
 
 %=========== Settings
-Method          = 1;
 preloadsecs     = [];
 pixelFormat     = 5;
 AudioVol        = 0;        
 StartTime       = 1;        % Start from 1st frame
 
 %=========== Load each clip
-wbh = waitbar(0);
+%wbh = waitbar(0);
 for c = 1:numel(Clips)
-    waitbar(c/numel(Clips), wbh, sprintf('Loading %d = %.2f MB... (%d/%d)',Clips(c).Filename,Clips(c).MemMB,c,numel(Clips)));
-    
+    %waitbar(c/numel(Clips), wbh, sprintf('Loading %s = %.2f MB... (%d/%d)',Clips(c).Filename,Clips(c).MemMB,c,numel(Clips)));
+    fprintf('Loading %s = %.2f MB... (%d/%d)',Clips(c).Filename,Clips(c).MemMB,c,numel(Clips));
+    if verbose == 1 && exist('PTBwin','var')
+        TextString = sprintf('Loading %s = %.2f MB... (%d/%d)',Clips(c).Filename,Clips(c).MemMB,c,numel(Clips));
+        DrawFormattedText(PTBwin, TextString, 200, 200, [1 1 1]*255);
+        Screen('Flip', PTBwin);  
+    end
+
     if Method == 1      %============== Use PTB 'GetMovieImage'
         [Clips(c).handle, Clips(c).TotalDuration, Clips(c).fps, width, height, Clips(c).count, Clips(c).AR] = Screen('OpenMovie', PTBwin, Clips(c).FullFilename, [], preloadsecs, [], pixelFormat);
         Clips(c).FrameDim       = [width, height];
         Clips(c).TotalFrames    = Clips(c).TotalDuration*Clips(c).fps;
         Screen('PlayMovie', Clips(c).handle, 1, [], AudioVol);
         Screen('SetmovieTimeIndex', Clips(c).handle, StartTime, 1); 
-        for f = 1%:Clips(c).TotalFrames
+        for f = 1:Clips(c).TotalFrames
             Clips(c).framehandles(f) = Screen('GetMovieImage', PTBwin, Clips(c).handle, 1);
         end
         
     elseif Method == 2  %============== Use PTB 'MakeTexture'
         video = mmread(Clips(c).FullFilename, [],[],[],true);           % read movie
         for f = 1:numel(video.frames)
-            Frames(:,:,:,f) = video.frames(f).cdata;
+            Clips(c).Frames(:,:,:,f) = video.frames(f).cdata;
         end
         Clips(c).TotalFrames    = video.nrFramesTotal;
         Clips(c).fps            = round(video.rate);
         Clips(c).FrameDim       = [video.width, video.height];
         Clips(c).TotalDuration  = Clips(c).TotalFrames/Clips(c).fps;
-        for f = 1:Clips(c).TotalFrames
-            Clips(c).framehandles(f) = Screen('MakeTexture', Frames(:,:,:,f));
-        end
+%         for f = 1:Clips(c).TotalFrames
+%             Clips(c).framehandles(f) = Screen('MakeTexture', Frames(:,:,:,f));
+%         end
     end
 end
-delete(wbh);
+%delete(wbh)
